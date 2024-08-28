8c724a919e99c0002899992ba272e3973d0210b1
Fergal Cotter <fbcotter90@gmail.com>
2024-08-28 13:31:32 +0100

    Disable flash attention

--- sam2/modeling/sam/transformer.py
+++ sam2/modeling/sam/transformer.py
@@ -16,10 +16,10 @@ from torch import nn, Tensor
 from sam2.modeling.position_encoding import apply_rotary_enc, compute_axial_cis
 
 from sam2.modeling.sam2_utils import MLP
-from sam2.utils.misc import get_sdpa_settings
+#  from sam2.utils.misc import get_sdpa_settings
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
-OLD_GPU, USE_FLASH_ATTN, MATH_KERNEL_ON = get_sdpa_settings()
+OLD_GPU, USE_FLASH_ATTN, MATH_KERNEL_ON = True, True, True
 
 
 class TwoWayTransformer(nn.Module):
